c9a55eadc066ec720d300fd5708e2d09d5d54392
daguttt <dagutmu667@gmail.com>
2025-08-11 15:22:51 -0500

    fix: change whatsapp number

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,3 @@
 // Centralized app constants
 // Phone numbers should be E.164-compatible without +, spaces, or dashes
-// export const WHATSAPP_NUMBER = '50688823775';
-export const WHATSAPP_NUMBER = '3042472138';
+export const WHATSAPP_NUMBER = '50688823775';
